23302d6fbca7a6c4f3353e7e6fdaf256dd0054d3
Brian Riant <brianriant@gmail.com>
2025-03-05 23:57:24 +0300

    ✅ resolve ts error

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { ArrowUp, BookOpen, Clock, Search, Share2 } from 'lucide-react';
+import { ArrowUp, BookOpen, Clock, Search } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { posts } from './post';
